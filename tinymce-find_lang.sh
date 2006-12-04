54a6a06ee7dc76e0a31426da6a796f2ecfd87891
Elan Ruusamäe <glen@pld-linux.org>
2006-12-04 15:05:13 +0000

    - not needed
    Changed files:
        tinymce-find_lang.sh -> 1.2

--- tinymce-find_lang.sh
+++
@@ -1,7 +0,0 @@
-find $RPM_BUILD_ROOT -type f -name '*.js' | sed '
-	s:'"$RPM_BUILD_ROOT"'::
-	s:\(.*/langs/\)\([^/_]\+\)\(.*\.js$\):%lang(\2) \1\2\3:
-	s:^\([^%].*\)::
-	s:%lang(C) ::
-	s:^\$::
-' | egrep -v '^$'
